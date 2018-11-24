5ab47db0c5d0e6a8ec284e18c2f8fb8b1a150b42
Lindsay M <lminami23@punahou.edu>
2018-11-24 11:00:11 -1000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function drag(e){
+	e.dataTransfer.setData('text', e.target.id);
+}
+
+function allowDrop(e){
+	e.preventDefault();
+}
+
+function drop(e) {
+	var data = e.dataTransfer.getData('text');
+	e.target.appendChild(document.getElementById(data));
+}
